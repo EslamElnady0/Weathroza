2b705e84404ca55a8aabbee480cbe9c6f241f3db
EslamElnady0 <eslamelnady254@gmail.com>
2026-02-17 02:17:52 +0200

    using the same function in body and preview

--- app/src/main/java/com/eslamdev/weathroza/presentaion/home/view/HomeBody.kt
+++ app/src/main/java/com/eslamdev/weathroza/presentaion/home/view/HomeBody.kt
@@ -41,8 +41,7 @@ import com.eslamdev.weathroza.presentaion.home.view.components.StatusItem
 
 @Composable
 fun HomeBody(bottomController: NavController, modifier: Modifier = Modifier) {
-    Column() {
-    }
+   HomeBodyImpl()
 }
 
 @Preview(
@@ -54,183 +53,188 @@ fun HomeBodyPreview(modifier: Modifier = Modifier) {
 
     MaterialTheme(colorScheme = darkColorScheme()) {
         Scaffold { innerPadding ->
-
-            LazyColumn(
-                modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                item {
-                    HeightSpacer(48.0)
-                    Text(
-                        stringResource(R.string.miami),
-                        fontSize = 30.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                    HeightSpacer(2.0)
-                    Text(
-                        stringResource(R.string.date_placeholder) + " • " + stringResource(R.string.time_placeholder),
-                        color = AppColors.lightGray
-                    )
-                    Image(
-                        painter = painterResource(id = R.drawable.dummy_sun_image),
-                        contentDescription = stringResource(R.string.weather_icon_desc),
-                        modifier = Modifier.height(130.dp)
-                    )
-                    Text(
-                        stringResource(R.string.clear_sky),
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Medium,
-                        color = AppColors.gray
-                    )
-                    DegreeText(
-                        stringResource(R.string.temp_placeholder), fontSize = 96.sp,
-                        color = AppColors.primary,
-                        fontWeight = FontWeight.ExtraBold
-                    )
-                    HeightSpacer(30.0)
-                    Row {
-                        StatusItem(
-                            icon = R.drawable.humidity_ic,
-                            label = stringResource(R.string.humidity),
-                            value = stringResource(R.string.humidity_value_placeholder),
-                            contentDesc = stringResource(R.string.humidity_icon_desc),
-                            modifier = Modifier.weight(1f)
-                        )
-                        WidthSpacer(8.0)
-                        StatusItem(
-                            icon = R.drawable.wind_ic,
-                            label = stringResource(R.string.wind),
-                            value = stringResource(R.string.wind_value_placeholder),
-                            contentDesc = stringResource(R.string.wind_icon_desc),
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-                    HeightSpacer(8.0)
-                    Row {
-                        StatusItem(
-                            icon = R.drawable.pressure,
-                            label = stringResource(R.string.pressure),
-                            value = stringResource(R.string.pressure_value_placeholder),
-                            contentDesc = stringResource(R.string.pressure_icon_desc),
-                            modifier = Modifier.weight(1f)
-                        )
-                        WidthSpacer(8.0)
-                        StatusItem(
-                            icon = R.drawable.cloud_ic,
-                            label = stringResource(R.string.clouds),
-                            value = stringResource(R.string.clouds_value_placeholder),
-                            contentDesc = stringResource(R.string.clouds_icon_desc),
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-                    HeightSpacer(12.0)
-                    CardWithBoarder {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            StatsRow(
-                                icon = R.drawable.sunrise_ic,
-                                label = stringResource(R.string.sunrise),
-                                value = stringResource(R.string.sunrise_time_placeholder),
-                                contentDesc = stringResource(R.string.sunrise_icon_desc),
-                            )
-                            WidthSpacer(8.0)
-                            VerticalDivider(
-                                Modifier.height(40.dp),
-                                DividerDefaults.Thickness,
-                                AppColors.primary.copy(alpha = 0.2f)
-                            )
-                            WidthSpacer(8.0)
-                            StatsRow(
-                                icon = R.drawable.sunset,
-                                label = stringResource(R.string.sunset),
-                                value = stringResource(R.string.sunset_time_placeholder),
-                                contentDesc = stringResource(R.string.sunset_icon_desc),
-                            )
-                        }
-                    }
-                    HeightSpacer(20.0)
-
-                    val sampleHourlyForecasts = listOf(
-                        HourlyForecast("10 AM", R.drawable.dummy_sun_image, "62", "Sunny"),
-                        HourlyForecast("11 AM", R.drawable.dummy_sun_image, "60", "Sunny"),
-                        HourlyForecast("12 PM", R.drawable.dummy_sun_image, "59", "Sunny"),
-                        HourlyForecast("01 PM", R.drawable.dummy_sun_image, "61", "Sunny"),
-                        HourlyForecast("02 PM", R.drawable.dummy_sun_image, "63", "Sunny")
-                    )
-                    HourlyForecastList(forecasts = sampleHourlyForecasts)
-
-                    HeightSpacer(24.0)
-                }
-
-                val sampleDailyForecasts = listOf(
-                    DailyForecast(
-                        "Tuesday",
-                        "Oct 24",
-                        R.drawable.dummy_sun_image,
-                        "Sunny",
-                        "19.56",
-                        "17.83"
-                    ),
-                    DailyForecast(
-                        "Wednesday",
-                        "Oct 25",
-                        R.drawable.dummy_sun_image,
-                        "Rainy",
-                        "16.20",
-                        "14.56"
-                    ),
-                    DailyForecast(
-                        "Thursday",
-                        "Oct 26",
-                        R.drawable.dummy_sun_image,
-                        "Stormy",
-                        "15.45",
-                        "13.02"
-                    ),
-                    DailyForecast(
-                        "Friday",
-                        "Oct 27",
-                        R.drawable.dummy_sun_image,
-                        "Cloudy",
-                        "18.12",
-                        "16.85"
-                    ),
-                    DailyForecast(
-                        "Saturday",
-                        "Oct 28",
-                        R.drawable.dummy_sun_image,
-                        "Sunny",
-                        "21.80",
-                        "20.50"
-                    ),
-                    DailyForecast(
-                        "Sunday",
-                        "Oct 29",
-                        R.drawable.dummy_sun_image,
-                        "Sunny",
-                        "22.30",
-                        "21.10"
-                    ),
-                    DailyForecast(
-                        "Monday",
-                        "Oct 30",
-                        R.drawable.dummy_sun_image,
-                        "Cloudy",
-                        "18.90",
-                        "17.20"
-                    )
-                )
-                item {
-                    SevenDayForecastList(forecasts = sampleDailyForecasts)
-                    HeightSpacer(24.0)
-                }
-            }
+            HomeBodyImpl(modifier.padding(innerPadding))
         }
     }
 
 }
+
+@Composable
+fun HomeBodyImpl(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            HeightSpacer(30.0)
+            Text(
+                stringResource(R.string.miami),
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+            HeightSpacer(2.0)
+            Text(
+                stringResource(R.string.date_placeholder) + " • " + stringResource(R.string.time_placeholder),
+                color = AppColors.lightGray
+            )
+            Image(
+                painter = painterResource(id = R.drawable.dummy_sun_image),
+                contentDescription = stringResource(R.string.weather_icon_desc),
+                modifier = Modifier.height(130.dp)
+            )
+            Text(
+                stringResource(R.string.clear_sky),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium,
+                color = AppColors.gray
+            )
+            DegreeText(
+                stringResource(R.string.temp_placeholder), fontSize = 96.sp,
+                color = AppColors.primary,
+                fontWeight = FontWeight.ExtraBold
+            )
+            HeightSpacer(16.0)
+            Row {
+                StatusItem(
+                    icon = R.drawable.humidity_ic,
+                    label = stringResource(R.string.humidity),
+                    value = stringResource(R.string.humidity_value_placeholder),
+                    contentDesc = stringResource(R.string.humidity_icon_desc),
+                    modifier = Modifier.weight(1f)
+                )
+                WidthSpacer(8.0)
+                StatusItem(
+                    icon = R.drawable.wind_ic,
+                    label = stringResource(R.string.wind),
+                    value = stringResource(R.string.wind_value_placeholder),
+                    contentDesc = stringResource(R.string.wind_icon_desc),
+                    modifier = Modifier.weight(1f)
+                )
+            }
+            HeightSpacer(8.0)
+            Row {
+                StatusItem(
+                    icon = R.drawable.pressure,
+                    label = stringResource(R.string.pressure),
+                    value = stringResource(R.string.pressure_value_placeholder),
+                    contentDesc = stringResource(R.string.pressure_icon_desc),
+                    modifier = Modifier.weight(1f)
+                )
+                WidthSpacer(8.0)
+                StatusItem(
+                    icon = R.drawable.cloud_ic,
+                    label = stringResource(R.string.clouds),
+                    value = stringResource(R.string.clouds_value_placeholder),
+                    contentDesc = stringResource(R.string.clouds_icon_desc),
+                    modifier = Modifier.weight(1f)
+                )
+            }
+            HeightSpacer(12.0)
+            CardWithBoarder {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    StatsRow(
+                        icon = R.drawable.sunrise_ic,
+                        label = stringResource(R.string.sunrise),
+                        value = stringResource(R.string.sunrise_time_placeholder),
+                        contentDesc = stringResource(R.string.sunrise_icon_desc),
+                    )
+                    WidthSpacer(8.0)
+                    VerticalDivider(
+                        Modifier.height(40.dp),
+                        DividerDefaults.Thickness,
+                        AppColors.primary.copy(alpha = 0.2f)
+                    )
+                    WidthSpacer(8.0)
+                    StatsRow(
+                        icon = R.drawable.sunset,
+                        label = stringResource(R.string.sunset),
+                        value = stringResource(R.string.sunset_time_placeholder),
+                        contentDesc = stringResource(R.string.sunset_icon_desc),
+                    )
+                }
+            }
+            HeightSpacer(20.0)
+
+            val sampleHourlyForecasts = listOf(
+                HourlyForecast("10 AM", R.drawable.dummy_sun_image, "62", "Sunny"),
+                HourlyForecast("11 AM", R.drawable.dummy_sun_image, "60", "Sunny"),
+                HourlyForecast("12 PM", R.drawable.dummy_sun_image, "59", "Sunny"),
+                HourlyForecast("01 PM", R.drawable.dummy_sun_image, "61", "Sunny"),
+                HourlyForecast("02 PM", R.drawable.dummy_sun_image, "63", "Sunny")
+            )
+            HourlyForecastList(forecasts = sampleHourlyForecasts)
+
+            HeightSpacer(24.0)
+        }
+
+        val sampleDailyForecasts = listOf(
+            DailyForecast(
+                "Tuesday",
+                "Oct 24",
+                R.drawable.dummy_sun_image,
+                "Sunny",
+                "19.56",
+                "17.83"
+            ),
+            DailyForecast(
+                "Wednesday",
+                "Oct 25",
+                R.drawable.dummy_sun_image,
+                "Rainy",
+                "16.20",
+                "14.56"
+            ),
+            DailyForecast(
+                "Thursday",
+                "Oct 26",
+                R.drawable.dummy_sun_image,
+                "Stormy",
+                "15.45",
+                "13.02"
+            ),
+            DailyForecast(
+                "Friday",
+                "Oct 27",
+                R.drawable.dummy_sun_image,
+                "Cloudy",
+                "18.12",
+                "16.85"
+            ),
+            DailyForecast(
+                "Saturday",
+                "Oct 28",
+                R.drawable.dummy_sun_image,
+                "Sunny",
+                "21.80",
+                "20.50"
+            ),
+            DailyForecast(
+                "Sunday",
+                "Oct 29",
+                R.drawable.dummy_sun_image,
+                "Sunny",
+                "22.30",
+                "21.10"
+            ),
+            DailyForecast(
+                "Monday",
+                "Oct 30",
+                R.drawable.dummy_sun_image,
+                "Cloudy",
+                "18.90",
+                "17.20"
+            )
+        )
+        item {
+            SevenDayForecastList(forecasts = sampleDailyForecasts)
+            HeightSpacer(24.0)
+        }
+    }
+}
+
