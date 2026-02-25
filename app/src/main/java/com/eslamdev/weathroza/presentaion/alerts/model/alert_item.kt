3ec32821442843dbeddeba87480d6fa9cc32d6d2
EslamElnady0 <eslamelnady254@gmail.com>
2026-02-26 00:38:23 +0200

    removed un used ui model

--- app/src/main/java/com/eslamdev/weathroza/presentaion/alerts/model/alert_item.kt
+++
@@ -1,9 +0,0 @@
-package com.eslamdev.weathroza.presentaion.alerts.model
-
-data class AlertItem(
-    val id: Int,
-    val title: String,
-    val timeRange: String,
-    val location: String,
-    val isEnabled: Boolean,
-)
